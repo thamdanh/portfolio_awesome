23562b3cad18bf41c4c640df84005be6ab759971
Telvin Nguyen <telvin.nguyen@theaccessgroup.com>
2025-07-07 16:39:15 +0700

    Separate velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-const isDev = process.argv.indexOf("dev") !== -1;
-const isBuild = process.argv.indexOf("build") !== -1;
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = "1";
-  const { build } = await import("velite");
-  await build({ watch: isDev, clean: !isDev });
-}
+
 
 console.log(`Next.config.ts print env variables`, process.env.NEXT_PUBLIC_SUPABASE_URL)
 
